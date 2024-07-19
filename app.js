7effadfc92c65db7d48c674e044a8efd0685c0db
sanjan B M <acharyasanjanbm@gmail.com>
2024-07-19 21:09:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  console.log("connected");
+});
+
+// Optional: Handle disconnection
+io.on("disconnect", function () {
+  console.log("User disconnected");
+});
+
+app.get("/", function (req, res) {
+  res.render("index"); // Ensure you have an index.ejs file in the views directory
+});
+
+server.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+const socket = io();
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
+
+const map = L.map("map").setView([0, 0], 16); //need to review
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap by sanjan",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+});
